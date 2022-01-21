6120e4dfec73500e4c4721e7a7e224c581c7a231
Tomasz Kornuta <tkornuta@gmail.com>
2022-01-20 17:49:52 -0800

    training - start
    Signed-off-by: Tomasz Kornuta <tkornuta@gmail.com>

--- nlp/sierra/bert2bert-goals.py
+++ nlp/sierra/bert2bert-goals.py
@@ -1,38 +1,38 @@
 # SPDX-License-Identifier: Apache-2.0
+# based on:
+# https://arxiv.org/pdf/1907.12461.pdf
+# https://huggingface.co/docs/transformers/model_doc/bertgeneration
+# https://medium.com/huggingface/encoder-decoders-in-transformers-a-hybrid-pre-trained-architecture-for-seq2seq-af4d7bf14bb8
+
+# SPDX-License-Identifier: Apache-2.0
 # based on: https://huggingface.co/docs/transformers/v4.15.0/en/internal/tokenization_utils#transformers.SpecialTokensMixin
 
 import os
-from transformers import  BertGenerationEncoder, BertGenerationDecoder, EncoderDecoderModel, BertTokenizer, PreTrainedTokenizerFast
-from transformers import AutoConfig, BertConfig
-from transformers import EncoderDecoderConfig
+
+from transformers import  BertGenerationEncoder, BertGenerationDecoder, EncoderDecoderModel
+from transformers import BertTokenizer, PreTrainedTokenizerFast
+from transformers import BertConfig
 
 brain_path = "/home/tkornuta/data/brain2"
 decoder_tokenizer_path = os.path.join(brain_path, "leonardo_sierra.decoder_tokenizer.json")
 
-# Let's see how to increase the vocabulary of Bert model and tokenizer
+# Load original BERT Ttokenizer.
 encoder_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-#decoder_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+# Load decoder operating on the Sierra PDDL language.
 decoder_tokenizer = PreTrainedTokenizerFast(tokenizer_file=decoder_tokenizer_path)
-print(len(decoder_tokenizer))
 
 # leverage checkpoints for Bert2Bert model...
 # use BERT's cls token as BOS token and sep token as EOS token
 encoder = BertGenerationEncoder.from_pretrained("bert-base-uncased",
-    bos_token_id=encoder_tokenizer.vocab["[CLS]"],
-    eos_token_id=encoder_tokenizer.vocab["[SEP]"],
+    # Set required tokens.
+    #bos_token_id=encoder_tokenizer.vocab["[CLS]"],
+    #eos_token_id=encoder_tokenizer.vocab["[SEP]"],
     )
-# add cross attention layers and use BERT's cls token as BOS token and sep token as EOS token
-#decoder = BertGenerationDecoder.from_pretrained("bert-base-uncased",
-#    add_cross_attention=True, is_decoder=True,
-#    bos_token_id=decoder_tokenizer.vocab["[CLS]"],
-#    eos_token_id=decoder_tokenizer.vocab["[SEP]"],
-#    )
-#decoder.resize_token_embeddings(len(decoder_tokenizer))
 
 # Fresh decoder config.
 decoder_config = BertConfig(
     is_decoder = True,
-    add_cross_attention = True,
+    add_cross_attention = True, # add cross attention layers
     vocab_size = len(decoder_tokenizer),
     # Set required tokens.
     unk_token_id = decoder_tokenizer.vocab["[UNK]"],
@@ -43,28 +43,22 @@ decoder_config = BertConfig(
     bos_token_id = decoder_tokenizer.vocab["[BOS]"],
     eos_token_id = decoder_tokenizer.vocab["[EOS]"],
     )
-# AutoConfig.from_pretrained("bert-base-uncased")
-#decoder_config = BertGenerationDecoderConfig()
-
-# From: https://github.com/huggingface/transformers/blob/master/src/transformers/models/encoder_decoder/modeling_encoder_decoder.py#L464
-#>>> model.config.decoder_start_token_id = tokenizer.cls_token_id
-#>>> model.config.pad_token_id = tokenizer.pad_token_id
-#>>> model.config.vocab_size = model.config.decoder.vocab_size
-#decoder_config.decoder_start_token_id = decoder_tokenizer.vocab["[CLS]"]
-# decoder_config.pad_token_type_id = 0 ?
+# Initialize a brand new bert-based decoder.
 decoder = BertGenerationDecoder(config=decoder_config)
 
-#enc_dec_config = EncoderDecoderConfig(encoder=encoder.config, decoder=decoder.config, decoder_start_token_id=decoder_tokenizer.vocab["[CLS]"])
-
+# Setup enc-decoder mode.
 bert2bert = EncoderDecoderModel(encoder=encoder, decoder=decoder)
 bert2bert.config.decoder_start_token_id=decoder_tokenizer.vocab["[CLS]"]
 bert2bert.config.pad_token_id=decoder_tokenizer.vocab["[PAD]"]
 
-# Tokenize inputs and labels.
-inputs = encoder_tokenizer('Make a stack of all blocks except the green block.', add_special_tokens=False, return_tensors="pt")
-print("Inputs: ", inputs)
-labels = decoder_tokenizer("has_anything(robot),on_surface(blue_block, tabletop),stacked(blue_block, red_block),on_surface(yellow_block, tabletop)", return_tensors="pt")
+# Sample.
+command = "Separate the given stack to form blue, red and yellow blocks stack."
+goals = "has_anything(robot),on_surface(blue_block, tabletop),stacked(blue_block, red_block),on_surface(yellow_block, tabletop)"
 
+# Tokenize inputs and labels.
+inputs = encoder_tokenizer(command, add_special_tokens=False, return_tensors="pt")
+print("Inputs: ", inputs)
+labels = decoder_tokenizer(goals, return_tensors="pt")
 
 # train...
 # v4.12.0: The decoder_input_ids are now created based on the labels, no need to pass them yourself anymore.
@@ -76,4 +70,3 @@ output.loss.backward()
 greedy_output = bert2bert.generate(inputs.input_ids, max_length=50)
 print(f"Output ({greedy_output.shape}): {greedy_output}")
 print(f"Detokenized: `{decoder_tokenizer.decode(greedy_output[0], skip_special_tokens=False)}`")
-
